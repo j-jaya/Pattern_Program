8c79abe7f24557697a2febd2063cb0377fee15df
j-jaya <jjakkappagol6@gmail.com>
2025-07-26 19:49:27 +0530

    Add files via upload

---
+++ PatternP1/src/com/gqt/pattern/Pattern1.java
@@ -0,0 +1,10 @@
+package com.gqt.pattern;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+	System.out.println("*");
+
+	}
+
+}
---
+++ PatternP9/src/com/gqt/pattern/Patternp9.java
@@ -0,0 +1,17 @@
+package com.gqt.pattern;
+import java.util.Scanner;
+public class Patternp9 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the count");
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=n;j++) {
+			System.out.print(i+" ");
+		}
+			System.out.println();	
+	}
+
+}
+}
