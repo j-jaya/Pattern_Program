a643ba17ec39809977e32684d87f3a03c8f8f7f7
j-jaya <jjakkappagol6@gmail.com>
2025-07-27 08:18:38 +0530

    Add files via upload

---
+++ PatternPP25/src/com/gqt/pattern25/Pattern25.java
@@ -0,0 +1,23 @@
+package com.gqt.pattern25;
+import java.util.Scanner;
+public class Pattern25 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the count");
+		int n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			
+			for(int j=i;j<=n;j++) {
+				System.out.print(" ");
+				
+					}
+			for(int j=1;j<=(2*i-1);j++) {
+				System.out.print(i+"");
+			}	
+			System.out.println();
+	
+		}	
+	}
+}
